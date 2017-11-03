93d4923bfde3e8c555849c5ea9a76eed60c0bc9c
Andrew Loiacono <jetsfan1991@gmail.com>
2017-11-03 20:16:21 +0000

    lot of updates still tons of work to do

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 A node script to calculate yearly earnings and track stock averages. Trade transaction data obtained from Robinhood API.
 
-This script relies on [robinhood-to-csv](https://github.com/joshfraser/robinhood-to-csv) to produce a csv file acceptable of parsing.
+This script relies on [robinhood-to-csv](https://github.com/joshfraser/robinhood-to-csv) to produce a csv file acceptable for parsing.
 
 #### Prerequisites:
-Node.js v6.11.2 or higher
+node.js v6.11.2 or higher
+
+csv file generated from [robinhood-to-csv](https://github.com/joshfraser/robinhood-to-csv) 
 
 #### Install:
 Download repository and then navigate to the root of repository folder.
--- index.js
+++ index.js
@@ -4,41 +4,67 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const parse = require('csv-parse');
 
-const question = {
-  type: 'input',
-  name: 'file',
-  message: 'Enter full path to csv file:'
-};
+let symbolDictionary = {};
 
-let transactionDictionary = {};
+function updateSymbolData(transaction) {
+  if (transaction.side === 'buy') {
+    
+    const symbol = symbolDictionary[transaction.symbol];
+    
+    const transaction_price = parseFloat(transaction.price);
+    const transaction_quantity = parseFloat(transaction.quantity);
+    
+    const new_total_price = symbol.total_price + (transaction_price*transaction_quantity);
+    const new_quantity = symbol.quantity + transaction_quantity;
+    
+    symbol.total_price = new_total_price;
+    symbol.quantity = new_quantity;
+    symbol.average_price = new_total_price / new_quantity;
+  }
+  // sell
+  else {
+    
+    const symbol = symbolDictionary[transaction.symbol];
+    
+    const transaction_price = parseFloat(transaction.price);
+    const transaction_quantity = parseFloat(transaction.quantity);
+    
+    const new_total_price = symbol.total_price - (transaction_price*transaction_quantity);
+    const new_quantity = symbol.quantity - transaction_quantity;
+    
+    symbol.total_price = new_total_price;
+    symbol.quantity = new_quantity;
+    symbol.average_price = new_total_price / new_quantity;
+  }
+}
 
-function addNewTransaction(transaction) {
-  if (transaction.state === 'buy') {
-    transactionDictionary[transaction.symbol] = {
-      'price': parseFloat(transaction.price),
+function addNewSymbol(transaction) {
+  if (transaction.side === 'buy') {
+    symbolDictionary[transaction.symbol] = {
+      'average_price': parseFloat(transaction.price),
+      'total_price': parseFloat(transaction.price),
       'quantity': parseFloat(transaction.quantity)
     };
   }
-  // why is the first transaction for this symbol a sell?
+  // why is the first transaction for this symbol a sell? Free stock...
   else {
-    console.log('why is the first transaction for '+transaction.symbol+' a sell?');
-    console.log('aborting transaction');
+    console.log('Why is the first transaction for '+transaction.symbol +' a sell? Aborting transaction...');
   }
 }
 
 function addOrUpdateFilledTransaction(transaction) {
   // symbol has previous transactions
-  if (transactionDictionary.hasOwnProperty(transaction.symbol)) {
-    
+  if (symbolDictionary.hasOwnProperty(transaction.symbol)) {
+    updateSymbolData(transaction);
   }
   // new transaction for this symbol
   else {
-    addNewTransaction(transaction);
+    addNewSymbol(transaction);
   }
 }
 
 function interateFilledTransactions(transactions) {
-  for (let i = transactions.length; i > 0; i--) {
+  for (let i = transactions.length-1; i > -1; i--) {
     const transaction = transactions[i];
     
     if (transaction.state === 'filled') {
@@ -46,6 +72,23 @@ function interateFilledTransactions(transactions) {
     }
   }
 }
+
+function logSymbolDictionary() {
+  for (let prop in symbolDictionary) {
+    const currentSymbol = symbolDictionary[prop];
+    console.log('---------');
+    console.log(prop);
+    console.log('Total Cost: '+currentSymbol.total_price);
+    console.log('Average Cost: '+currentSymbol.average_price);
+    console.log('Quantity: '+currentSymbol.quantity);
+  }
+}
+
+const question = {
+  type: 'input',
+  name: 'file',
+  message: 'Enter full path to csv file:'
+};
 
 inquirer.prompt([question]).then(function (answer) {
   const transactions = [];
@@ -56,5 +99,7 @@ inquirer.prompt([question]).then(function (answer) {
     })
     .on('end',function() {
       interateFilledTransactions(transactions);
+      console.log('Below is a list of your Symbols:');
+      logSymbolDictionary();
     });
 });
